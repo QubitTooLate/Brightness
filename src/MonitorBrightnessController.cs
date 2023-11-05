679d21027cffc9bf8ad03619de8f5b9835cd3848
Jester <58484964+QubitTooLate@users.noreply.github.com>
2023-11-05 01:30:52 +0100

    Initial code

---
+++ src/MonitorBrightnessController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Qtl.BrightnessController;
+
+public static unsafe partial class MonitorBrightnessController
+{
+	public static float GetPrimaryMonitorBrightness()
+	{
+		var primaryMonitorHandle = GetPrimaryMonitorHandle();
+		var physicalPrimaryMonitorHandle = GetPhysicalMonitorHandle(primaryMonitorHandle);
+
+		try
+		{
+			return GetMonitorBrightness(physicalPrimaryMonitorHandle);
+		}
+		finally
+		{
+			DestroyPhysicalMonitorHandle(physicalPrimaryMonitorHandle);
+		}
+	}
+
+	public static void SetPrimaryMonitorBrightness(float value)
+	{
+		var primaryMonitorHandle = GetPrimaryMonitorHandle();
+		var physicalPrimaryMonitorHandle = GetPhysicalMonitorHandle(primaryMonitorHandle);
+
+		try
+		{
+			SetMonitorBrightness(physicalPrimaryMonitorHandle, value);
+		}
+		finally
+		{
+			DestroyPhysicalMonitorHandle(physicalPrimaryMonitorHandle);
+		}
+	}
+
+	private static IntPtr GetPrimaryMonitorHandle()
+	{
+		const int MONITOR_DEFAULTTOPRIMARY = 1;
+		var point = default(POINT);
+		return MonitorFromPoint(ref point, MONITOR_DEFAULTTOPRIMARY);
+	}
+
+	private static float GetMonitorBrightness(IntPtr physicalMonitorHandle)
+	{
+		if (!GetMonitorBrightness(physicalMonitorHandle, out var min, out var current, out var max))
+		{
+			ThrowForLastError();
+		}
+
+		return (current - min) / (float)(max - min);
+	}
+
+	private static void SetMonitorBrightness(IntPtr physicalMonitorHandle, float value)
+	{
+		if (!GetMonitorBrightness(physicalMonitorHandle, out var min, out var current, out var max))
+		{
+			ThrowForLastError();
+		}
+
+		var newValue = min + (uint)MathF.Floor((max - min) * value);
+
+		if (!SetMonitorBrightness(physicalMonitorHandle, newValue))
+		{
+			ThrowForLastError();
+		}
+	}
+
+	private static IntPtr GetPhysicalMonitorHandle(IntPtr monitorHandle)
+	{
+		var physicalMonitors = stackalloc PHYSICAL_MONITOR[PHYSICAL_MONITOR_COUNT];
+		if (!GetPhysicalMonitorsFromHMONITOR(monitorHandle, PHYSICAL_MONITOR_COUNT, physicalMonitors))
+		{
+			throw new NotImplementedException("Only supports one physical monitor per monitor handle.");
+		}
+
+		return physicalMonitors[0].hPhysicalMonitor;
+	}
+
+	private static void DestroyPhysicalMonitorHandle(IntPtr physicalMonitorHandle)
+	{
+		var physicalMonitors = stackalloc PHYSICAL_MONITOR[PHYSICAL_MONITOR_COUNT];
+		physicalMonitors[0].hPhysicalMonitor = physicalMonitorHandle;
+		_ = DestroyPhysicalMonitors(PHYSICAL_MONITOR_COUNT, physicalMonitors);
+	}
+
+	private static void ThrowForLastError() => throw Marshal.GetExceptionForHR(Marshal.GetHRForLastWin32Error())!;
+
+	private const int PHYSICAL_MONITOR_COUNT = 1;
+
+	[LibraryImport("User32.dll", SetLastError = true)]
+	private static partial IntPtr MonitorFromPoint(ref POINT point, int flags);
+
+	[StructLayout(LayoutKind.Sequential)]
+	private struct POINT
+	{
+		public int X;
+		public int Y;
+	}
+
+	[LibraryImport("Dxva2.dll", SetLastError = true)]
+	[return: MarshalAs(UnmanagedType.Bool)]
+	private static partial bool GetNumberOfPhysicalMonitorsFromHMONITOR(
+		IntPtr hMonitor,
+		out uint pdwNumberOfPhysicalMonitors);
+
+	[LibraryImport("Dxva2.dll", StringMarshalling = StringMarshalling.Utf16, SetLastError = true)]
+	[return: MarshalAs(UnmanagedType.Bool)]
+	private static partial bool GetPhysicalMonitorsFromHMONITOR(
+		IntPtr hMonitor,
+		uint dwPhysicalMonitorArraySize,
+		PHYSICAL_MONITOR* pPhysicalMonitorArray);
+
+	[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+	private struct PHYSICAL_MONITOR
+	{
+		public IntPtr hPhysicalMonitor;
+		public fixed char szPhysicalMonitorDescription[128];
+	}
+
+	[LibraryImport("Dxva2.dll", SetLastError = true)]
+	[return: MarshalAs(UnmanagedType.Bool)]
+	private static partial bool GetMonitorBrightness(
+		IntPtr hMonitor,
+		out uint pdwMinimumBrightness,
+		out uint pdwCurrentBrightness,
+		out uint pdwMaximumBrightness);
+
+
+	[LibraryImport("Dxva2.dll", SetLastError = true)]
+	[return: MarshalAs(UnmanagedType.Bool)]
+	private static partial bool SetMonitorBrightness(
+		IntPtr hMonitor,
+		uint brightness);
+
+	[LibraryImport("Dxva2.dll", SetLastError = true)]
+	[return: MarshalAs(UnmanagedType.Bool)]
+	private static partial bool DestroyPhysicalMonitors(
+		uint dwPhysicalMonitorArraySize,
+		PHYSICAL_MONITOR* pPhysicalMonitorArray);
+}
---
+++ src/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Qtl.BrightnessController;
+using Qtl.Keylogging.HotKeys;
+
+const int VK_F2 = 0x71;
+const int VK_F3 = 0x72;
+
+const float BRIGHTNESS_STEP = 0.1f;
+
+MonitorBrightnessController.SetPrimaryMonitorBrightness(0.0f);
+
+using var hotKeys = HotKeyTask.StartNew();
+
+var brightness = 0.0f;
+
+hotKeys.AddHotKey(HotKeyModifiers.Win, VK_F2, hotKey =>
+{
+	if (brightness >= BRIGHTNESS_STEP)
+	{
+		brightness -= BRIGHTNESS_STEP;
+		MonitorBrightnessController.SetPrimaryMonitorBrightness(brightness);
+		WriteBrightness(brightness);
+	}
+});
+
+hotKeys.AddHotKey(HotKeyModifiers.Win, VK_F3, hotKey =>
+{
+	if (brightness <= 1.0f)
+	{
+		brightness += BRIGHTNESS_STEP;
+		MonitorBrightnessController.SetPrimaryMonitorBrightness(brightness);
+		WriteBrightness(brightness);
+	}
+});
+
+Console.ReadKey();
+
+await hotKeys.StopAsync();
+
+return;
+
+static void WriteBrightness(float brightness) => Console.WriteLine($"{brightness:P1} {new string('|', (int)(brightness * 100.0f))}");
